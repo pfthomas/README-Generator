b2fd3710347cb7d7ef340132a776c8c09f3bf14d
pftho <pf.thomas97@gmail.com>
2022-09-07 21:25:52 -0400

    Code functioning properly, readme generator works

---
+++ README.md
@@ -0,0 +1,32 @@
+
+    # README Generator
+    
+    ## Description
+    Prompts the user for information about their project and creates a README.md file for them to use
+    # Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributions](#contributions)
+    * [Tests](#tests)
+    * [Questions](#questions)
+
+    ## Installation
+    To install necessary dependencies, run the following command:
+    npm i
+
+    ## Usage
+    It is run through node.js
+
+    ## License 
+    
+    ## Contributions
+    N/A
+
+    ## Tests
+    N/A
+    ##Questions 
+    Reach out for questions at:
+    Github: https://github.com/pfthomas
+    Email: pf.thomas97@gmail.com
+    
---
+++ index.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const questions = [{
+    type: "input",
+    message: "What is your project's name?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "Please write a short description of your project",
+    name: "Description",
+}, {
+    type: "list",
+    message: "What kind of license should your project have",
+    name: "License",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+}, {
+    type: "input",
+    message: "What command should be run to install dependencies",
+    name: "Dependencies",
+}, {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "Tests",
+}, {
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "Usage"
+}, {   
+    type: "input", 
+    message: 'What is your Github username?',
+    name: "Github"    
+}, {
+    type: "input",
+    message: "What does the user need to know about contributions to the repo?",
+    name: "Contributions",
+}, {
+    type: "input",
+    message: 'What is your email address?',
+    name: 'Email'
+},
+];
+
+const generateReadMe = (data) => {
+    return `
+    # ${data.Title}
+    ${data.License}
+    ## Description
+    ${data.Description}
+    # Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributions](#contributions)
+    * [Tests](#tests)
+    * [Questions](#questions)
+
+    ## Installation
+    To install necessary dependencies, run the following command:
+    ${data.Dependencies}
+
+    ## Usage
+    ${data.Usage}
+
+    ## License 
+    ${data.License}
+    ## Contributions
+    ${data.Contributions}
+
+    ## Tests
+    ${data.Tests}
+    ##Questions 
+    Reach out for questions at:
+    Github: https://github.com/${data.Github}
+    Email: ${data.Email}
+    `
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        
+        console.log(fileName);
+        console.log(data);
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("README generated");
+        }
+    })
+}
+
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data){
+            if(data.License == "MIT") {
+                data.License = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";  
+            }
+            if (data.License == "APACHE 2.0"){
+                data.License ="[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            }
+            if (data.License == "GPL 3.0"){
+                data.License = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"; 
+            }
+            if (data.License == "BSD 3"){
+                data.License = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"; 
+            }
+            if (data.License == "None"){
+                data.License = '';
+            }
+            writeToFile("README.md", generateReadMe(data));
+            console.log(data)
+        })
+}
+
+init();
